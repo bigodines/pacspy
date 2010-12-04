37f6e281f30cf5f94734c34207daa4a4d5d7f658
matheus mendes <bigodines@yahoo.com.br>
2010-12-04 14:47:07 -0200

    added censo json

--- src/main.py
+++ src/main.py
@@ -8,17 +8,22 @@ def format_value(old_val):
 	return new_val
 
 
-reader = csv.reader(open('resources/PAC_SP_por_cidade.csv','rb'))
-writer = csv.writer(open('resources/PAC_com_valor_normalizado.csv', 'wb'))
+# reader = csv.reader(open('resources/PAC_SP_por_cidade.csv','rb'))
+# writer = csv.writer(open('resources/PAC_com_valor_normalizado.csv', 'wb'))
 
-keys = ["area", "municipio", "proponente", "tipo", "data", "valor", "estagio"]
-modified_data = []
+# keys = ["area", "municipio", "proponente", "tipo", "data", "valor", "estagio"]
+# pac_data = []
+# for line in reader:
+# 	data = {}
+# 	line[5] = format_value(line[5])
+# 	out = dict(zip(keys, line))
+# 	pac_data.append(out)
+
+
+reader = csv.reader(open('resources/censo2010_SP.csv','rb'))
+keys = ["cod","mesorregiao", "nome_mesorregiao", "microrregiao", "nome_microrregiao", "cidade", "uf", "1970", "1980", "1981", "2000", "2007", "populacao", "homens", "mulheres", "urbana", "rural", "crescimento"]
+censo_data = []
 for line in reader:
-	data = {}
-	line[5] = format_value(line[5])
-	out = dict(zip(keys, line))
-	modified_data.append(out)
-
-
+	censo_data.append(dict(zip(keys,line)))
 	
-print json.dumps(modified_data, indent=4)
+print json.dumps(censo_data, indent=4)
